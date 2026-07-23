83273adef12fbdd05b49bd2b243fe45488193497
mambo <83398131+longestcow@users.noreply.github.com>
2026-07-23 00:53:04 -0600

    init commit

---
+++ Assets/scripts/FirstPersonController.cs
@@ -0,0 +1,223 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class FirstPersonController : MonoBehaviour
+{
+	[Header("Player")]
+	public float MoveSpeed = 4.0f;
+	public float SprintSpeed = 6.0f;
+	public float RotationSpeed = 1.0f;
+	public float SpeedChangeRate = 10.0f;
+
+	[Space(10)]
+	public float JumpHeight = 1.2f;
+	public float Gravity = -9.8f;
+
+	[Space(10)]
+	public float JumpTimeout = 0.1f;
+	public float FallTimeout = 0.15f;
+
+	[Header("Player Grounded")]
+	public bool Grounded = true;
+	public float GroundedOffset = -0.14f;
+	public float GroundedRadius = 0.5f;
+	public LayerMask GroundLayers;
+
+	[Header("Cinemachine")]
+	public GameObject CinemachineCameraTarget;
+	public float TopClamp = 90.0f;
+	public float BottomClamp = -90.0f;
+
+	// cinemachine
+	private float _cinemachineTargetPitch;
+
+	// player
+	private float _speed;
+	private float _rotationVelocity;
+	private float _verticalVelocity;
+	private float _terminalVelocity = 53.0f;
+
+	// timeout deltatime
+	private float _jumpTimeoutDelta;
+	private float _fallTimeoutDelta;
+
+
+	private PlayerInput _playerInput;
+	private CharacterController _controller;
+	private StarterAssetsInputs _input;
+	private GameObject _mainCamera;
+
+	private const float _threshold = 0.01f;
+
+	private bool IsCurrentDeviceMouse
+	{
+		get
+		{
+			return _playerInput.currentControlScheme == "KeyboardMouse";
+
+		}
+	}
+
+	private void Awake()
+	{
+		// get a reference to our main camera
+		if (_mainCamera == null)
+		{
+			_mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+		}
+	}
+
+	private void Start()
+	{
+		_controller = GetComponent<CharacterController>();
+		_input = GetComponent<StarterAssetsInputs>();
+		_playerInput = GetComponent<PlayerInput>();
+
+
+		_jumpTimeoutDelta = JumpTimeout;
+		_fallTimeoutDelta = FallTimeout;
+	}
+
+	private void Update()
+	{
+		JumpAndGravity();
+		GroundedCheck();
+		Move();
+	}
+
+	private void LateUpdate()
+	{
+		CameraRotation();
+	}
+
+	private void GroundedCheck()
+	{
+		// set sphere position, with offset
+		Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
+		Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
+	}
+
+	private void CameraRotation()
+	{
+		// if there is an input
+		if (_input.look.sqrMagnitude >= _threshold)
+		{
+			//Don't multiply mouse input by Time.deltaTime
+			float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+			
+			_cinemachineTargetPitch += _input.look.y * RotationSpeed * deltaTimeMultiplier;
+			_rotationVelocity = _input.look.x * RotationSpeed * deltaTimeMultiplier;
+
+			// clamp our pitch rotation
+			_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+			// Update Cinemachine camera target pitch
+			CinemachineCameraTarget.transform.localRotation = Quaternion.Euler(_cinemachineTargetPitch, 0.0f, 0.0f);
+
+			// rotate the player left and right
+			transform.Rotate(Vector3.up * _rotationVelocity);
+		}
+	}
+
+	private void Move()
+	{
+		// set target speed based on move speed, sprint speed and if sprint is pressed
+		float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+
+		// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+		// if there is no input, set the target speed to 0
+		if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+
+		// a reference to the players current horizontal velocity
+		float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+		float speedOffset = 0.1f;
+		float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+		// accelerate or decelerate to target speed
+		if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+		{
+			_speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
+			_speed = Mathf.Round(_speed * 1000f) / 1000f;
+		}
+		else
+		{
+			_speed = targetSpeed;
+		}
+
+		// normalise input direction
+		Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+		if (_input.move != Vector2.zero)
+			inputDirection = transform.right * _input.move.x + transform.forward * _input.move.y;
+		
+
+		_controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+	}
+
+	private void JumpAndGravity()
+	{
+		if (Grounded)
+		{
+			_fallTimeoutDelta = FallTimeout;
+
+			// stop our velocity dropping infinitely when grounded
+			if (_verticalVelocity < 0.0f){
+				_verticalVelocity = -2f;
+			}
+
+			if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+			{
+				// the square root of H * -2 * G = how much velocity needed to reach desired height
+				_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+			}
+
+			// jump timeout
+			if (_jumpTimeoutDelta >= 0.0f)
+			{
+				_jumpTimeoutDelta -= Time.deltaTime;
+			}
+		}
+		else
+		{
+			// reset the jump timeout timer
+			_jumpTimeoutDelta = JumpTimeout;
+
+			// fall timeout
+			if (_fallTimeoutDelta >= 0.0f)
+			{
+				_fallTimeoutDelta -= Time.deltaTime;
+			}
+
+			// if we are not grounded, do not jump
+			_input.jump = false;
+		}
+
+		if (_verticalVelocity < _terminalVelocity)
+		{
+			_verticalVelocity += Gravity * Time.deltaTime;
+		}
+	}
+
+	private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+	{
+		if (lfAngle < -360f) lfAngle += 360f;
+		if (lfAngle > 360f) lfAngle -= 360f;
+		return Mathf.Clamp(lfAngle, lfMin, lfMax);
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+		Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+		if (Grounded) Gizmos.color = transparentGreen;
+		else Gizmos.color = transparentRed;
+
+		// when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
+		Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
+	}
+}
